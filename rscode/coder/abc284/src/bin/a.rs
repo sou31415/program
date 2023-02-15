99c48050f2dae5d3118cb12ce1f678c9fd8deffb
sou31415 <tbc3141@icloud.com>
2023-02-15 21:46:56 +0900

    add ab284a abc288c

--- rscode/coder/abc284/src/bin/a.rs
+++ rscode/coder/abc284/src/bin/a.rs
@@ -1,6 +1,7 @@
 //submit passed
-use proconio::input;
-
+#[allow(unused_imports)]
+use proconio::{fastout, input};
+#[fastout]
 fn main() {
     input! {
         n:usize,
--- rscode/coder/abc288/src/bin/c.rs
+++ rscode/coder/abc288/src/bin/c.rs
@@ -1,8 +1,17 @@
 use petgraph::unionfind::UnionFind;
-use proconio::input;
+use proconio::{input, marker::Usize1};
 fn main() {
     input! {
-        n:usize,m:usize,
+        n:usize,m:usize,ab:[(Usize1 , Usize1);m],
     }
-    result = UnionFind::new();
+    let mut result = UnionFind::new(n);
+    let mut ans: usize = 0;
+    for (a, b) in ab {
+        if result.equiv(a, b) {
+            ans += 1;
+        } else {
+            result.union(a, b);
+        }
+    }
+    println!("{}", ans);
 }
