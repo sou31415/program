c9f77e13e44de8c7ce4c1636c5ff7eae95ae3c8b
sou31415 <tbc3141@icloud.com>
2023-03-11 22:42:30 +0900

    add abc293

--- rscode/coder/abc293/src/bin/b.rs
+++ rscode/coder/abc293/src/bin/b.rs
@@ -1,12 +1,25 @@
 #[allow(unused_imports)]
-use proconio::{fastout , input , marker::Usize1 , marker::Chars};
-#[allow(unused_imports)]
 use petgraph::unionfind::UnionFind;
 #[allow(unused_imports)]
-use std::collections::{HashSet , VecDeque};
+use proconio::{fastout, input, marker::Chars, marker::Usize1};
+#[allow(unused_imports)]
+use std::collections::{HashSet, VecDeque};
 
 #[fastout]
-fn main(){
-    input!{}
-    println!("{}" , );
+fn main() {
+    input! {
+        n:usize,a:[Usize1;n],
+    }
+    let mut v: Vec<bool> = vec![false; n];
+    for i in 0..n {
+        if !v[i] {
+            v[a[i]] = true;
+        }
+    }
+    println!("{}", v.iter().filter(|x| x == &&false).count());
+    for i in 0..n {
+        if !v[i] {
+            print!("{} ", i + 1);
+        }
+    }
 }
--- rscode/coder/abc293/src/bin/c.rs
+++ rscode/coder/abc293/src/bin/c.rs
@@ -1,12 +1,23 @@
 #[allow(unused_imports)]
-use proconio::{fastout , input , marker::Usize1 , marker::Chars};
-#[allow(unused_imports)]
 use petgraph::unionfind::UnionFind;
 #[allow(unused_imports)]
-use std::collections::{HashSet , VecDeque};
+use proconio::{fastout, input, marker::Chars, marker::Usize1};
+#[allow(unused_imports)]
+use std::collections::{HashSet, VecDeque};
 
 #[fastout]
-fn main(){
-    input!{}
-    println!("{}" , );
+fn main() {
+    input! {
+        h:usize , w:usize,
+        ab:[[usize;w];h],
+    }
+    let mut count:usize = 0;
+    let mut dp:Vec<Vec<usize>> = vec![vec![0;w];h];
+    dp[0] = vec![1_usize;w];
+    for i in 0..h{
+        dp[i][0] = 1;
+    }
+    println!("{}",count);
 }
+
+fn search(v:&Vec<Vec<usize>> )
