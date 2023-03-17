0dc87d7ed5937dc780d3d1b8029718040df6da00
sou31415 <tbc3141@icloud.com>
2023-03-17 21:29:22 +0900

    add abc287_c

--- rscode/coder/abc287/src/bin/c.rs
+++ rscode/coder/abc287/src/bin/c.rs
@@ -1,3 +1,33 @@
+use petgraph::unionfind::UnionFind;
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n:usize , m:usize,
+        uv:[(Usize1 , Usize1);m],
+    }
+    let mut edge: Vec<Vec<usize>> = vec![vec![]; n];
+    let mut uf = UnionFind::new(n);
+    if n - 1 != m {
+        println!("No");
+        return;
+    }
+    for (a, b) in uv {
+        if !uf.equiv(a, b) {
+            uf.union(a, b);
+            edge[a].push(b);
+            edge[b].push(a);
+        } else {
+            println!("No");
+            return;
+        }
+    }
+    if edge.iter().filter(|x| x.len() == 2).count() == n - 2
+        && edge.iter().filter(|x| x.len() == 1).count() == 2
+    {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
