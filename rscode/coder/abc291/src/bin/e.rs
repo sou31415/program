19b1971b6ae87c6966e0224309f0aaa51346bd2e
sou31415 <tbc3141@icloud.com>
2023-03-23 22:12:54 +0900

    solving abc291_e

--- rscode/coder/abc291/src/bin/e.rs
+++ rscode/coder/abc291/src/bin/e.rs
@@ -1,9 +1,18 @@
-use proconio::{fastout, input};
+use petgraph::{
+    algo::{toposort, DfsSpace},
+    graph::DiGraph,
+};
+use proconio::{fastout, input, marker::Usize1};
+
 #[fastout]
 fn main() {
     input! {
-        n:usize , m:usize ,
-        xy:[(Usize1 , Usize1);m],
+        n:usize , m:usize,
+        xy:[(Usize1 , Usize1);n],
     }
-    let mut rt: Vec<usize> = (0..n).collect();
+    let mut graph: Vec<Vec<usize>> = vec![vec![]; n];
+    let g: DiGraph<usize, usize> = DiGraph::from_edges(&xy);
+    let mut space = DfsSpace::new(&g);
+    let result = toposort(&g, Some(&mut space));
+    let nodes: Vec<usize> = result.unwrap().iter().map(|x| x.index()).collect();
 }
