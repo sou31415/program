5e438d606a09433538053609aaaf53b564a95c69
sou31415 <tbc3141@icloud.com>
2023-03-24 11:18:35 +0900

    a

--- rscode/coder/abc295/src/bin/f.rs
+++ rscode/coder/abc295/src/bin/f.rs
@@ -1,3 +1,12 @@
-fn main() {
-    unimplemented!();
+#[allow(unused_imports)]
+use proconio::{fastout , input , marker::Usize1 , marker::Chars};
+#[allow(unused_imports)]
+use petgraph::unionfind::UnionFind;
+#[allow(unused_imports)]
+use std::collections::{HashSet , VecDeque};
+
+#[fastout]
+fn main(){
+    input!{}
+    println!("{}" , );
 }
