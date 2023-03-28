1123b288525bccff1b7c7b53e8c4672a7e9336fe
sou31415 <tbc3141@icloud.com>
2023-03-28 19:59:30 +0900

    add abc284_c short

--- rscode/coder/abc284/src/bin/c.rs
+++ rscode/coder/abc284/src/bin/c.rs
@@ -1,16 +1,18 @@
-use petgraph::unionfind::UnionFind;
 use proconio::{input, marker::Usize1};
-use std::collections::HashSet;
-#[proconio::fastout]
 fn main() {
     input! {
         n :usize , m:usize,
         ab:[(Usize1 , Usize1);m],
     }
-    let mut uf = UnionFind::new(n);
+    let mut uf = petgraph::unionfind::UnionFind::new(n);
     for (a, b) in ab {
         uf.union(a, b);
     }
-    let set = uf.into_labeling().into_iter().collect::<HashSet<usize>>();
-    println!("{}", set.len());
+    println!(
+        "{}",
+        uf.into_labeling()
+            .into_iter()
+            .collect::<std::collections::HashSet<_>>()
+            .len()
+    );
 }
