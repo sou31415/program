f674f97ad20904581abbfe3b43622962940d547c
sou31415 <tbc3141@icloud.com>
2023-03-28 18:49:24 +0900

    add abc293_c

--- rscode/coder/abc293/src/bin/c.rs
+++ rscode/coder/abc293/src/bin/c.rs
@@ -11,10 +11,34 @@ fn main() {
         h:usize , w:usize,
         ab:[[usize;w];h],
     }
-    let mut count:usize = 0;
-
-    println!("{}",count);
+    let count = dfs(h, w, ab);
+    println!("{}", count);
 }
 
-fn dfs(v:Vec<Vec<usize>> , count:&mut usize ,h:usize , w:usize) {
-
+fn dfs(h: usize, w: usize, g: Vec<Vec<usize>>) -> usize {
+    let mut set: HashSet<usize> = HashSet::new();
+    let mut count: usize = 0;
+    let mut stack: Vec<(usize, usize, HashSet<usize>)> = vec![];
+    set.insert(g[0][0]);
+    stack.push((0, 0, set.clone()));
+    while let Some((x, y, mut set)) = stack.pop() {
+        if x < (w - 1) && y < h {
+            let mut sets = set.clone();
+            sets.insert(g[y][x + 1]);
+            stack.push((x + 1, y, sets.clone()));
+        }
+        if y < (h - 1) && x < w {
+            let mut sets = set.clone();
+            sets.insert(g[y + 1][x]);
+            stack.push((x, y + 1, sets.clone()));
+        }
+        if x == w - 1 && y == h - 1 {
+            set.insert(g[h - 1][w - 1]);
+            if set.len() == h + w - 1 {
+                count += 1;
+            }
+            set.clear();
+        }
+    }
+    count
+}
