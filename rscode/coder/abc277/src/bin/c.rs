8e8509f288dd851dc49ddd82f0d6eb361fab13f1
sou31415 <tbc3141@icloud.com>
2023-04-01 12:56:16 +0900

    fix dfs

--- rscode/coder/abc277/src/bin/c.rs
+++ rscode/coder/abc277/src/bin/c.rs
@@ -3,7 +3,22 @@ use proconio::{fastout, input, marker::Usize1};
 #[fastout]
 fn main() {
     input! {
-        n:usize,ab[(Usize1 , Usize1);n],
+        n:usize,
+        ab:[(Usize1 , Usize1);n],
     }
-    let mut g: Vec<Vec<usize>> = vec![];
+
+    let mut g: Vec<Vec<usize>> = vec![vec![];1000000001];
+    for (a ,b) in ab{
+        g[a].push(b);
+        g[b].push(a);
+    }
+    let mut ans:usize = dfs(mut g);
 }
+
+fn dfs(g:mut Vec<Vec<usize>>) -> usize{
+    let mut  v:Vec<usize> = vec![];
+    while let Some(x) = g[0].pop(){
+        v.push(x);
+    }
+    while let Some(x) = v.pop(){
+
--- rscode/dfs/src/main.rs
+++ rscode/dfs/src/main.rs
@@ -1,4 +1,3 @@
-//submitted abc293_c
 #[allow(unused_imports)]
 use petgraph::unionfind::UnionFind;
 #[allow(unused_imports)]
@@ -9,23 +8,37 @@ use std::collections::{HashSet, VecDeque};
 #[fastout]
 fn main() {
     input! {
-        n:usize , m:usize,
-        a:[usize;n],
-        b:[usize;m],
+        h:usize , w:usize,
+        ab:[[usize;w];h],
     }
-    let mut v: Vec<usize> = vec![];
-    for i in 0..n {
-        v.push(a[i]);
+    let count = dfs(h, w, ab);
+    println!("{}", count);
+}
+
+fn dfs(h: usize, w: usize, g: Vec<Vec<usize>>) -> usize {
+    let mut set: HashSet<usize> = HashSet::new();
+    let mut count: usize = 0;
+    let mut stack: Vec<(usize, usize, HashSet<usize>)> = vec![];
+    set.insert(g[0][0]);
+    stack.push((0, 0, set.clone()));
+    while let Some((x, y, mut set)) = stack.pop() {
+        if x < (w - 1) && y < h {
+            let mut sets = set.clone();
+            sets.insert(g[y][x + 1]);
+            stack.push((x + 1, y, sets.clone()));
+        }
+        if y < (h - 1) && x < w {
+            let mut sets = set.clone();
+            sets.insert(g[y + 1][x]);
+            stack.push((x, y + 1, sets.clone()));
+        }
+        if x == w - 1 && y == h - 1 {
+            set.insert(g[h - 1][w - 1]);
+            if set.len() == h + w - 1 {
+                count += 1;
+            }
+            set.clear();
+        }
     }
-    for i in 0..m {
-        v.push(b[i]);
-    }
-    v.sort();
-    for i in 0..a.len() {
-        print!("{} ", v.binary_search(&a[i]).unwrap() + 1);
-    }
-    println!("");
-    for i in 0..b.len() {
-        print!("{} ", v.binary_search(&b[i]).unwrap() + 1);
-    }
+    count
 }
