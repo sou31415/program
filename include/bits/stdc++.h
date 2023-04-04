0da23316379dfa6c372d828fb0f02f7ba1ac2fbd
sou31415 <tbc3141@icloud.com>
2023-04-05 00:29:11 +0900

    add stdc++.h

---
+++ include/bits/stdc++.h
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <string> 
+#include <vector> 
+#include <algorithm> 
+#include <utility> 
+#include <tuple> 
+#include <cstdint> 
+#include <cstdio> 
+#include <map> 
+#include <queue> 
+#include <set> 
+#include <stack> 
+#include <deque> 
+#include <unordered_map> 
+#include <unordered_set> 
+#include <bitset>
+#include <cctype> 
