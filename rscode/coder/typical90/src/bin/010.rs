e4dea303d05247ef62316799e4bb1fc9bfd48a55
sou31415 <tbc3141@icloud.com>
2023-04-22 14:11:31 +0900

    add typical90/10

--- rscode/coder/typical90/src/bin/010.rs
+++ rscode/coder/typical90/src/bin/010.rs
@@ -1,3 +1,32 @@
+#[allow(unused_imports)]
+use proconio::{fastout, input, marker::Usize1};
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+      n:usize,ab:[(usize,usize);n],
+    }
+    let mut imos1: Vec<usize> = vec![0];
+    let mut imos2: Vec<usize> = vec![0];
+
+    for (a, b) in ab {
+        if a == 1 {
+            let nlen: usize = imos1.len() - 1;
+            imos1.push(imos1[nlen] + b);
+            imos2.push(imos2[imos2.len() - 1]);
+        } else {
+            let nlen: usize = imos2.len() - 1;
+            imos2.push(imos2[nlen] + b);
+            imos1.push(imos1[imos1.len() - 1]);
+        }
+    }
+    input! {
+      q:usize,
+    }
+    for _ in 0..q {
+        input! {
+          l:usize , r:usize,
+        }
+        println!("{} {}", imos1[r] - imos1[l - 1], imos2[r] - imos2[l - 1]);
+    }
 }
