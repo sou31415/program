3cc37660c9c1f627b13b0ccb0dbe696b8bde8cf8
sou31415 <tbc3141@icloud.com>
2023-04-24 17:42:27 +0900

    add abc285_d

--- rscode/coder/abc285/src/bin/d.rs
+++ rscode/coder/abc285/src/bin/d.rs
@@ -1,4 +1,4 @@
-use petgraph::{algo::is_cyclic_directed, prelude::DiGraphMap};
+use petgraph::unionfind::UnionFind;
 use proconio::{fastout, input};
 
 #[fastout]
@@ -7,9 +7,21 @@ fn main() {
         n: usize,
         s: [(String, String); n]
     }
-
-    let s = s.iter().map(|(x, y)| (x.as_str(), y.as_str()));
-    let g: DiGraphMap<_, ()> = s.collect();
-
-    println!("{}", if is_cyclic_directed(&g) { "No" } else { "Yes" });
+    let mut v: Vec<String> = Vec::new();
+    let mut uf: UnionFind<usize> = UnionFind::new(2 * n);
+    for (a, b) in &s {
+        v.push(a.to_string());
+        v.push(b.to_string());
+    }
+    v.sort();
+    v.dedup();
+    for (a, b) in &s {
+        if uf.equiv(v.binary_search(&a).unwrap(), v.binary_search(&b).unwrap()) {
+            println!("No");
+            return;
+        } else {
+            uf.union(v.binary_search(&a).unwrap(), v.binary_search(&b).unwrap());
+        }
+    }
+    println!("Yes");
 }
