1e2a393c69f7d62ca3629c4ed9ae5337f401f761
sou31415 <tbc3141@icloud.com>
2023-04-28 11:08:46 +0900

    fix dec program

--- rscode/dec/src/main.rs
+++ rscode/dec/src/main.rs
@@ -1,10 +1,10 @@
 use proconio::input;
 fn main() {
     input! {
-        n:usize,a:[usize;n],
+        s:String,
     }
-    for i in a {
-        print!("{:0>8b} ", i);
+    for i in s.split('.').map(|x| x.parse::<usize>().unwrap()) {
+        print!("{:0>8b}.", i);
     }
     println!("");
 }
