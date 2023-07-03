d5e4f22eaf72b3022c624a6a1343e8308d332e43
sou31415 <tbc3141@icloud.com>
2023-07-03 22:52:15 +0900

    add abc276_d

--- rscode/coder/abc276/src/bin/b.rs
+++ rscode/coder/abc276/src/bin/b.rs
@@ -1,22 +1,20 @@
+use itertools::Itertools;
 use proconio::{fastout, input, marker::Usize1};
-
-#[fastout]
 fn main() {
     input! {
-        n:usize , m:usize ,
-        ab:[(Usize1 , Usize1);m],
+      n:usize,m:usize,
+      ab:[(Usize1,Usize1);m]
     }
-    let mut data: Vec<Vec<usize>> = vec![vec![]; n];
-    for &(a, b) in &ab {
-        data[a].push(b);
-        data[b].push(a);
+    let mut g: Vec<Vec<usize>> = vec![vec![]; n];
+    for (a, b) in ab {
+        g[a].push(b);
+        g[b].push(a);
     }
     for i in 0..n {
-        data[i].sort();
-        print!("{} ", data[i].len());
-        for j in 0..data[i].len() {
-            print!("{} ", data[i][j] + 1);
-        }
-        println!("");
+        g[i].sort();
+    }
+    for i in 0..n {
+        print!("{} ", g[i].len());
+        println!("{}", g[i].iter().map(|x| *x + 1).join(" "));
     }
 }
--- rscode/coder/abc276/src/bin/d.rs
+++ rscode/coder/abc276/src/bin/d.rs
@@ -1,3 +1,49 @@
+use proconio::{fastout, input};
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+      n:usize,mut a:[usize;n],
+
+    }
+    let mut ans: usize = 0;
+    let mut s = a.clone();
+    s.sort();
+    s.dedup();
+    if s.len() == 1 {
+        println!("0");
+        return;
+    }
+    let q = s[0];
+    for i in 1..n {
+        if a[i] % q != 0 {
+            println!("-1");
+            return;
+        } else {
+            if q == a[i] {
+                continue;
+            }
+            a[i] /= q;
+            while a[i] >= 2 {
+                if a[i] / 2 >= q && a[i] % 2 == 0 {
+                    a[i] /= 2;
+                    ans += 1;
+                }
+                if a[i] == 1 {
+                    break;
+                }
+                if a[i] % 3 == 0 && a[i] / 3 >= q {
+                    a[i] /= 3;
+                    ans += 1;
+                }
+                if a[i] == 1 {
+                    break;
+                }
+                if a[i] % 2 != 0 && a[i] % 3 != 0 {
+                    println!("-1");
+                    return;
+                }
+            }
+        }
+    }
+    println!("{}", ans);
 }
