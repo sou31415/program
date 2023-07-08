f15a60884c459ab68664405e2b0b785edbf7b237
sou31415 <tbc3141@icloud.com>
2023-07-08 18:36:30 +0900

    add abc298_d

--- rscode/coder/abc298/src/bin/d.rs
+++ rscode/coder/abc298/src/bin/d.rs
@@ -11,7 +11,31 @@ fn main() {
     input! {
         q:usize,
     }
+    let mut result: usize = 1;
     let mut que: VecDeque<usize> = VecDeque::new();
     que.push_back(1);
-    let mut dp: Vec<Vec<usize>> = vec![10];
+    let mut dp: Vec<usize> = vec![1];
+    for i in 1..=600000 {
+        let len = dp.len();
+        let m = (dp[len - 1] * 10) % MOD;
+        dp.push(m);
+    }
+    for _ in 0..q {
+        input! {
+            a:usize,
+        }
+        if a == 1 {
+            input! {
+                x:usize
+            }
+            que.push_back(x);
+            result = (result * 10 + x) % MOD;
+        } else if a == 2 {
+            let len = que.len();
+            let r = que.pop_front().unwrap();
+            result = (result + 998244353 - (r * dp[len - 1]) % MOD) % MOD;
+        } else {
+            println!("{}", result);
+        }
+    }
 }
