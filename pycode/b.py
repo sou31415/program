16766c93b797931e503c4b5c00d4fd01e0676c7b
sou31415 <tbc3141@icloud.com>
2023-10-05 21:25:34 +0900

    rm python script

--- pycode/b.py
+++
@@ -1,7 +0,0 @@
-d = '.ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-a , b = map(int ,input().split())
-for i in range(a):
-    c = list(map(int ,input().split(' ')))
-    for j in c:
-        print(d[j] , end='')
-    print()
--- pycode/joi2023yoc.py
+++
@@ -1,3 +0,0 @@
-a = int(input())
-b = int(input())
-print(10 * a + b)
