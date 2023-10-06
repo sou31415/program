92427ecd2a20eb1d21649cc1776be2b4eeb3e59f
sou31415 <tbc3141@icloud.com>
2023-10-06 15:56:34 +0900

    modified abc304_c

--- rscode/coder/abc304/src/bin/c.rs
+++ rscode/coder/abc304/src/bin/c.rs
@@ -1,52 +1,46 @@
-#[allow(unused_imports)]
-use itertools::Itertools;
-#[allow(unused_imports)]
-use petgraph::unionfind::UnionFind;
-#[allow(unused_imports)]
-use proconio::{fastout, input, marker::Chars, marker::Usize1};
-#[allow(unused_imports)]
+use proconio::{fastout, input};
 use std::cmp::{max, min};
-#[allow(unused_imports)]
-use std::collections::{BTreeSet, HashSet, VecDeque};
+
 #[fastout]
 fn main() {
     input! {
-        n:usize,d:usize,
-        xy:[(isize,isize);n]
-    }
+    n:usize,d:usize,
+        ab:[(isize,isize);n]
+
+              }
     let mut g: Vec<Vec<usize>> = vec![vec![]; n];
     for i in 0..(n - 1) {
         for j in (i + 1)..n {
-            let q = (xy[i].0 - xy[j].0).abs() * (xy[i].0 - xy[j].0).abs()
-                + (xy[i].1 - xy[j].1).abs() * (xy[i].1 - xy[j].1).abs();
-            if d * d >= q as usize {
-                g[i].push(j);
+            if abser(ab[j], ab[i]) <= (d * d) as isize {
                 g[j].push(i);
+                g[i].push(j);
             }
         }
     }
-    let mut v = vec![false; n];
-    dfs(n, &mut g, &mut v);
+    let dis = dfs(n, g);
     for i in 0..n {
-        if v[i] {
+        if dis[i] {
             println!("Yes");
         } else {
             println!("No");
         }
     }
 }
-
-fn dfs(n: usize, g: &mut Vec<Vec<usize>>, seen: &mut Vec<bool>) {
+fn abser(x: (isize, isize), y: (isize, isize)) -> isize {
+    (x.0 - y.0) * (x.0 - y.0) + (x.1 - y.1) * (x.1 - y.1)
+}
+fn dfs(n: usize, mut g: Vec<Vec<usize>>) -> Vec<bool> {
+    let mut seen: Vec<bool> = vec![false; n];
     let mut stack: Vec<usize> = vec![];
-    seen[0] = true;
     while let Some(x) = g[0].pop() {
         stack.push(x);
-        seen[x] = true;
     }
+    seen[0] = true;
     while let Some(x) = stack.pop() {
         while let Some(y) = g[x].pop() {
             stack.push(y);
-            seen[y] = true;
         }
+        seen[x] = true;
     }
+    seen
 }
