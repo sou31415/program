9e933205c3828d1026077c9df12ac6de1bedd2db
sou31415 <tbc3141@icloud.com>
2023-11-21 11:13:57 +0900

    add abc329

--- rscode/coder/abc284/src/bin/a.rs
+++ rscode/coder/abc284/src/bin/a.rs
@@ -1,14 +1,11 @@
-//submit passed
-use petgraph::unionfind::UnionFind;
-#[allow(unused_imports)]
-use proconio::{fastout, input, marker::Chars};
-#[fastout]
+use proconio::input;
+
 fn main() {
     input! {
-        n:usize,
-        strlist:[String;n],
+        n:usize,mut s:[String;n]
     }
+    s.reverse();
     for i in 0..n {
-        println!("{}", strlist[n - i - 1]);
+        println!("{}", s[i]);
     }
 }
